831e60a8b56d894d092574634f04b44da7f98f8a
LeShaurya <raoljishauryadeep@gmail.com>
2025-01-21 16:14:26 +0530

    added test folder

---
+++ src/test/java/com/book_app/service/BookServiceTest.java
@@ -0,0 +1,41 @@
+package com.book_app.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class BookServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllBooks() {
+    }
+
+    @Test
+    void addBook() {
+    }
+
+    @Test
+    void deleteBook() {
+    }
+
+    @Test
+    void updateBook() {
+    }
+
+    @Test
+    void getBookById() {
+    }
+}
