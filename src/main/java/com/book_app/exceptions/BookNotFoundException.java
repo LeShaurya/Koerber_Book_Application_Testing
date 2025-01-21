d5ac84da752de9758f62db47c1721985f477801e
LeShaurya <raoljishauryadeep@gmail.com>
2025-01-21 10:15:34 +0530

    exception type changed

--- src/main/java/com/book_app/exceptions/BookNotFoundException.java
+++ src/main/java/com/book_app/exceptions/BookNotFoundException.java
@@ -1,6 +1,6 @@
 package com.book_app.exceptions;
 
-public class BookNotFoundException extends RuntimeException {
+public class BookNotFoundException extends Exception {
     public BookNotFoundException(String message) {
         super(message);
     }
